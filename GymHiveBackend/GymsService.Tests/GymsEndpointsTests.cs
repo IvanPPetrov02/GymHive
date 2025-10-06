af4e726c203870ab1e833904354ca71ea060e97e
Ivan Petrov <ivan.p.petrov02@gmail.com>
2025-10-06 11:03:29 +0300

    ci update

--- GymHiveBackend/GymsService.Tests/GymsEndpointsTests.cs
+++ GymHiveBackend/GymsService.Tests/GymsEndpointsTests.cs
@@ -1,2 +1,13 @@
+using Xunit;
 
+namespace GymsService.Tests;
 
+public class GymsEndpointsTests
+{
+    [Fact]
+    public void Placeholder_should_pass()
+    {
+        // TODO: Replace with real endpoint tests once minimal APIs / controllers are implemented.
+        Assert.True(true);
+    }
+}
--- GymHiveBackend/IdentityService.Tests/IdentityEndpointsTests.cs
+++ GymHiveBackend/IdentityService.Tests/IdentityEndpointsTests.cs
@@ -1,2 +1,13 @@
+using Xunit;
 
+namespace IdentityService.Tests;
 
+public class IdentityEndpointsTests
+{
+    [Fact]
+    public void Placeholder_should_pass()
+    {
+        // TODO: Replace with real identity endpoint tests when implemented.
+        Assert.True(true);
+    }
+}
